875c33b5895a53265b7a1cacd42ed7963c60eb86
DenZhigalov <denzhigalov@contorra.com>
2025-01-07 02:48:01 +0700

    tmp

--- src/newapp/simulation/spatial_hash/pointer_hash.rs
+++ src/newapp/simulation/spatial_hash/pointer_hash.rs
@@ -56,4 +56,67 @@ impl<Grid: SpatialGrid> PointerHash<Grid> {
         let end = self.pointers[cell_index + 1];
         &self.indexes[start..end]
     }
+
+    pub fn reference<'a, I>(&'a self, items: &'a mut [I]) -> HashReference<'a, I, Grid> {
+        let ptr = items.as_mut_ptr() as usize;
+        let cell_iterator = self.pointers.windows(2).map(|cell| {
+            self.indexes[cell[0]..cell[1]].iter().map(|&index| {
+                unsafe { &mut *(ptr as *mut I).add(index) };
+            })
+        });
+        HashReference::new(items, &self.grid, &self.pointers, &self.indexes)
+    }
+}
+
+pub struct HashReference<'a, I, Grid: SpatialGrid> {
+    data: &'a mut [I],
+    indexes: &'a [usize],
+    pointers: &'a [usize],
+    grid: &'a Grid,
+    start_cell_index: usize,
+}
+
+impl<'a, I, Grid: SpatialGrid> HashReference<'a, I, Grid> {
+    pub(self) fn new(
+        data: &'a mut [I],
+        grid: &'a Grid,
+        pointers: &'a [usize],
+        indexes: &'a [usize],
+    ) -> Self {
+        Self {
+            data,
+            pointers,
+            indexes,
+            grid,
+            start_cell_index: 0,
+        }
+    }
+
+    pub fn split_at_mut(self, coord: UVec2) -> (Self, Self) {
+        let Self {
+            data,
+            pointers,
+            indexes,
+            grid,
+            start_cell_index,
+        } = self;
+
+        let second_start_cell_index = grid.get_cell_index(coord);
+
+        // let (first_pointers, second_pointers) = pointers.split_at(mid);
+        // let (first_indexes, second_indexes) = indexes.split_at(second_star );
+
+        let first = Self {
+            data,
+            grid,
+            start_cell_coord,
+        };
+
+        let second = Self {
+            data,
+            grid,
+            start_cell_coord: coord,
+        };
+        (first, second)
+    }
 }
